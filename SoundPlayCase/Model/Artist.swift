785efab6b1d2228c0976834c7f418cb2d9386831
Onur Sapan <onursapan@hotmail.com>
2023-03-07 00:19:35 +0300

    Network Task
    Network task added.
    Model Created

--- SoundPlayCase/Model/Artist.swift
+++ SoundPlayCase/Model/Artist.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+struct Artist: Codable{
+    let resultCount: Int?
+    let results: [Result]?
+}
+
+// MARK: - Result
+struct Result: Codable {
+    let wrapperType, kind: String?
+    let collectionID, trackID: Int?
+    let artistName, collectionName, trackName, collectionCensoredName: String?
+    let trackCensoredName: String?
+    let collectionArtistID: Int?
+    let collectionArtistViewURL: String?
+    let collectionViewURL, trackViewURL: String?
+    let previewURL: String?
+    let artworkUrl30, artworkUrl60, artworkUrl100: String?
+    let collectionPrice, trackPrice: Double?
+    let trackRentalPrice, collectionHDPrice, trackHDPrice, trackHDRentalPrice: Double?
+    let releaseDate: String?
+    let collectionExplicitness, trackExplicitness: String?
+    let discCount, discNumber: Int?
+    let trackCount, trackNumber, trackTimeMillis: Int?
+    let country, currency, primaryGenreName: String?
+    let contentAdvisoryRating, shortDescription, longDescription: String?
+    let hasITunesExtras: Bool?
+    let artistID: Int?
+    let artistViewURL: String?
+    let isStreamable: Bool?
+}
---
+++ SoundPlayCase/ViewModel/HomeViewModel.swift
@@ -0,0 +1,47 @@
+//
+//  HomeViewModel.swift
+//  SoundPlayCase
+//
+//  Created by Onur Sapan on 3/6/23.
+//
+
+import Foundation
+
+class HomeViewModel{
+    
+    var onDataLoaded: (() -> Void)?
+    var onErrorReceived: ( (Error) -> Void)?
+    
+    var pageLimit: Int = 20
+    var offset: Int = 0
+    
+    var result: Artist?
+    
+    func viewDidLoad(){
+        fetchData()
+    }
+    
+    func fetchData(shouldGetNextPage: Bool = false){
+        offset = shouldGetNextPage ? offset + 1 : offset
+        
+        let url = URL(string: "https://itunes.apple.com/search?term=jack+johnson&limit=\(pageLimit)&offset=\(offset)")!
+
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else {
+                self.onErrorReceived?(error!)
+                print("Error: \(error?.localizedDescription ?? "Unknow Error")")
+                return
+            }
+            
+            do {
+                let response = try JSONDecoder().decode(Artist.self, from: data)
+                self.result = response
+                self.onDataLoaded?()
+            } catch {
+                self.onErrorReceived?(error)
+                print("Error: \(error.localizedDescription)")
+            }
+        }
+        task.resume()
+    }
+}
